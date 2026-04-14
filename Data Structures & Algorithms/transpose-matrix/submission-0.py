7e5647fcf3140a512fdb9805562a7f25fb74a40b
Atay Moya <112656287+ataymo1@users.noreply.github.com>
2026-04-14 12:37:05 -0700

    Add: transpose-matrix - submission-0

---
+++ Data Structures & Algorithms/transpose-matrix/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        
+        t_matrix = [[0 for i in range(len(matrix))] for j in range(len(matrix[0]))]
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                t_matrix[j][i] = matrix[i][j]
+        
+        return t_matrix
