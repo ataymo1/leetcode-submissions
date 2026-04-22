0a122a5e511a9dc2a82a406f5f5e4c46a5440c04
Atay Moya <112656287+ataymo1@users.noreply.github.com>
2026-04-22 15:56:37 -0700

    Add: bitwise-and-of-numbers-range - submission-2

---
+++ Data Structures & Algorithms/bitwise-and-of-numbers-range/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        
+        shifts = 0
+        while left < right:
+            left >>= 1
+            right >>= 1
+            shifts += 1
+
+        left <<= shifts
+
+        return left
+        
